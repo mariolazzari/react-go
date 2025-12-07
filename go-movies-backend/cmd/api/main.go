45d7ed83a7c383b74b5ca20cad6c9a83f21916b5
Mario Lazzari <mario.lazzari@gmail.com>
2025-12-07 18:40:34 +0100

    Lesson 48

--- go-movies-backend/cmd/api/main.go
+++ go-movies-backend/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,14 +10,16 @@ import (
 const port = 8080
 
 type application struct {
+	DSN    string
 	Domain string
 }
 
 func main() {
 	// app config
 	var app application
-
 	// command ling args
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// connct db
 
