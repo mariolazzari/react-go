080af552e68cc2c8a939a1a1b5cfa19f223f1c9f
Mario Lazzari <mario.lazzari@gmail.com>
2025-12-08 08:35:59 +0100

    Done lesson 49

--- go-movies-backend/cmd/api/main.go
+++ go-movies-backend/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -10,6 +11,7 @@ import (
 const port = 8080
 
 type application struct {
+	DB     *sql.DB
 	DSN    string
 	Domain string
 }
@@ -18,16 +20,21 @@ func main() {
 	// app config
 	var app application
 	// command ling args
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=mario password=password dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
 	flag.Parse()
 
 	// connct db
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
 
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 	// start web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
