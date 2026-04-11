82771bf0ef04ef4050fae942fae44b725d2dfd2f
Mario Lazzari <mario.lazzari@gmail.com>
2026-04-11 10:32:48 +0200

    Done lesson 80

--- go-movies-frontend/src/components/Genres.js
+++ go-movies-frontend/src/components/Genres.js
@@ -1,12 +1,10 @@
-
 const Genres = () => {
-
-    return(
-        <div>
-            <h2>Genres</h2>
-            <hr />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Genres</h2>
+      <hr />
+    </div>
+  );
+};
 
 export default Genres;
